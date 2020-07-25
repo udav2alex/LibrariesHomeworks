bca12e1747cb2359dd4b477cfa15d11913294705
udav2alex <udav2alex@gmail.com>
2020-07-25 12:02:09 +0300

    Initial commit

---
+++ app/src/main/java/ru/gressor/librarieshomeworks/homework2/MainActivityHW2_1.java
@@ -0,0 +1,16 @@
+package ru.gressor.librarieshomeworks.homework2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.gressor.librarieshomeworks.R;
+
+public class MainActivityHW2_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
